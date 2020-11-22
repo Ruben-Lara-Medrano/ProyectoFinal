18666391e4f946613d18c9ed441b8b96c7b5ad4a
Ruben Lara <rlaram01@educantabria.es>
2020-11-22 15:45:31 +0100

    funcion poner foto desde galeria

--- app/src/main/java/com/example/proyectofinal/perfil.java
+++ app/src/main/java/com/example/proyectofinal/perfil.java
@@ -1,57 +1,67 @@
 package com.example.proyectofinal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class perfil extends AppCompatActivity {
 private TextView tvPerfil;
+ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-        Toast.makeText(this, "perfil", Toast.LENGTH_SHORT).show();
-        tvPerfil=(TextView)findViewById(R.id.tvPerfil);
-        registerForContextMenu(tvPerfil);
+        TextView textView = findViewById(R.id.text_view);
+        registerForContextMenu(textView);
+        imagen= (ImageView) findViewById(R.id.iVFotoPerfil);
     }
-
-    public boolean onCreateOptionsMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
-    {
-
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.menu_contextual, menu );
-        return true;
+        menu.setHeaderTitle("Elige una opcion");
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
     }
-
-
-
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.copiar:
-                Toast.makeText(this, "Copiado", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.opcion1:
+                Toast.makeText(this, "Cambio de texto", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.editar:
-                Toast.makeText(this, "editar", Toast.LENGTH_SHORT).show();
+            case R.id.opcion2:
+                Toast.makeText(this, "Borrado de texto", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.pegar:
-                Toast.makeText(this, "pegar", Toast.LENGTH_SHORT).show();
-                return true;
-
             default:
                 return super.onContextItemSelected(item);
         }
+    }
 
-        // Id de cada opcion del menu
-//        if(id == R.id.paginaPrincipal){
-//
-//        }
+    public void cambiarFoto(View view) {
+        cargarImagen();
+    }
 
+    private void cargarImagen() {
+        Intent btnCambiarImagen = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        btnCambiarImagen.setType("image/");
+        startActivityForResult(btnCambiarImagen.createChooser(btnCambiarImagen,"Selecciona una imagen"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
     }
 }
