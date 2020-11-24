7a6453fd6abe5fb321d10d394e50910c126f8b89
Ruben Lara <rlaram01@educantabria.es>
2020-11-24 18:34:54 +0100

    Correcciones de errores y configuracion portrait definida.

--- app/src/main/java/com/example/proyectofinal/login.java
+++ app/src/main/java/com/example/proyectofinal/login.java
@@ -3,6 +3,7 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
     protected void onStart() {
         super.onStart();
